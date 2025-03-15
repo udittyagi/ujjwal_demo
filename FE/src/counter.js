b06e4adef871db0c31292299a64d5382baa0d7e0
Udit Tyagi <udittyagi2@gmail.com>
2025-03-15 19:38:45 +0530

    logs added

--- FE/src/counter.js
+++ FE/src/counter.js
@@ -15,6 +15,7 @@ async function fetchData() {
 
     //Parsing
     const jsonResp = await resp.json();
+    console.log("My Respionse+++++", jsonResp);
   } catch (err) {
     console.log("Errr++++", err);
   }
